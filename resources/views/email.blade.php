a0a9d869bd5114c38b5df0ee5306296231e673a8
RifkiEr24 <rifkierlangga17@gmail.com>
2021-03-03 03:39:35 +0700

    sanctum and email link redirect change for prod

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -8,7 +8,7 @@
 <div style="padding: 4rem; margin-left: auto; margin-right: auto; display: block; background-color: whitesmoke; border-radius: 25px;">
 <h2>{{ $judul }}</h2>
 <p>{{ $deskripsi }}</p>
-<a href="http://127.0.0.1:8000/{{$role}}/event"><button type="button" style="color: white; border: 0px; padding: 1rem; border-radius: 25px; background-color: rgb(83, 83, 196);">Pergi Lihat Jadwal</button></a>
+<a href="https://kepegawaiansmk1cmh.herokuapp.com//{{$role}}/event"><button type="button" style="color: white; border: 0px; padding: 1rem; border-radius: 25px; background-color: rgb(83, 83, 196);">Pergi Lihat Jadwal</button></a>
 </div>
 </div> 
 <div style="background-color: #2b1673; height: 75px;">
